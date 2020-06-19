ec054ae6e1119a2621e7848055d6dfaf516c09c6
Fayme Shahriar <fayme.shahriar@gmail.com>
2020-06-19 19:40:49 +0200

    relative path added

--- bigdata.py
+++ bigdata.py
@@ -2,15 +2,17 @@ from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
-import operator
+import os
+
 
 #Filter data to find only country with Australia
 def filterData(df):
       return df.filter(col("Country/Region") == "Australia")
 
+#Relative path
+dirname = os.path.dirname(__file__)
+filepath = os.path.join(dirname, 'time_series_19-covid-Confirmed_archived_0325.csv')
 
- 
-filepath = "/Users/FAY/Bigdata/Covid19-Big-Data" 
 sc = SparkContext("local", "covid_big_data")
 sqlContext = SQLContext(sc)
 df = sqlContext.read.option("inferSchema", "true").option("header", "true").csv(filepath)
